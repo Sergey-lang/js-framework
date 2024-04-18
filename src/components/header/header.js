b4ac4448c93b3113d16e6614d5deb52d77fa2b15
Siarhei <siarhei.kukharonak@startmatter.com>
2024-04-18 22:28:52 +0300

    - add header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from '../../common/div-component.js';
+import './header.css'
 
 export class Header extends DivComponent {
     constructor(appState) {
@@ -11,7 +12,20 @@ export class Header extends DivComponent {
         this.el.classList.add('header');
         this.el.innerHTML = `
             <div>
-                <img src="https://static.vecteezy.com/system/resources/previews/023/654/784/non_2x/golden-logo-template-free-png.png" alt="logo" />
+                <img src="/static/logo.svg" alt="logo" />
+            </div>
+            <div class="menu">
+                <a class="menu__item" href="#">
+                   <img src="/static/search.svg" alt="search" /> 
+                   Book search
+                </a>
+                  <a class="menu__item" href="#favorites">
+                   <img src="/static/favorites.svg" alt="search" /> 
+                   Favorites
+                </a>
+                <div class="menu__counter">
+                    ${this.appState.favorites.length}
+                </div>
             </div>
         `;
 
